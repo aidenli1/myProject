a8af20cdc8156cbc538d8310a8a8ab0b955f1e18
hongjingyu <hongjingyu@localhost>
2022-03-03 15:26:28 +0900

    f

--- src/com/human/ex/명함_06.java
+++ src/com/human/ex/명함_06.java
@@ -3,7 +3,7 @@ package com.human.ex;
 public class 명함_06 {
 
 	public static void main(String[] args) {
-	System.out.println("홍진규\n이메일 : myjingu@naver.com\n전화번호 : 010-8553-0140");
+	System.out.println("이름 : 홍진규\n이메일 : myjingu@naver.com\n전화번호 : 010-8553-0140");
 
 	}
 
