424a83b9fc44ef9334e202938e0a7ad9ad32c010
hongjingyu <hongjingyu@218.38.137.28>
2022-03-04 12:43:25 +0900

    3.4 금 >.<

---
+++ src/com/human/ex/_4일_06.java
@@ -0,0 +1,15 @@
+package com.human.ex;
+
+public class _4일_06 {
+
+	public static void main(String[] args) {
+		
+	String st1 = "Hello"; String st2 = "World";
+	System.out.println(st1);
+	System.out.println(st1 + "World");
+	System.out.println(st1 + st2);
+	String str3 = st1 + st2;
+	System.out.println(str3);
+	}
+
+}
---
+++ src/com/human/ex/_4일_07.java
@@ -0,0 +1,42 @@
+package com.human.ex;
+
+public class _4일_07 {
+
+	public static void main(String[] args) {
+		//문제 1
+////	String str = "이름 : 홍길동"; String str2 = "나이 : 29";
+//	String str3 = "키 : 170";
+//	System.out.println(str + str2 +str3);
+		
+		//문제 2
+		/*
+		정수자료형 : byte,short,int,long
+		상수자료형 : 
+		문자자료형 : char
+		불리언자료형 : boolean
+			*/
+		//문제 3
+//		10 =
+//		1.= double형
+//		6F= float형 상수 
+//		23d=double 상수 
+//		14E10= double형 E 표기법 
+//		'a'= char상수 형
+		
+//문제 4	
+//int a = 5; int b = 10;
+//	System.out.println(a + 3);
+//	System.out.println(a + b);
+//	
+		
+//	int 3i = 5; /x
+//	int i3a = 5; /o
+//	int __hello = 5; / o
+//	int helli=5.3; / x
+//	int j+i=4; / x
+//	int public = 5; /x
+//	int i#2 = 5; /x
+//	int MYCATAGE = 10; /x
+		
+	}
+}
